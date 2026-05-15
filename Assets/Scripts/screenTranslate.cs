d60400ed9e490364558082b8cdc97ec345b160f2
Zomblack3 <santi260406@gmail.com>
2026-05-15 13:50:41 -0300

    Update main menu
    Add settings button
    Add exit button
    Add exit function

--- Assets/Scripts/screenTranslate.cs
+++ Assets/Scripts/screenTranslate.cs
@@ -1,10 +1,3 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class ScreenTranslate : MonoBehaviour
-{
-    public void LoadNewLevel(string levelName)
-    {
-        SceneManager.LoadScene(levelName);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a55e189d2854222552479cb994c78dd1d0ef3373cc5ad4b2d5a2ccb474d4e1b
+size 376
