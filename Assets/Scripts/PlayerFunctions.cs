e9c0a54ae94647e6cd99b9d7cf544e8291433afd
Zomblack3 <santi260406@gmail.com>
2026-05-18 00:48:56 -0300

    Update gameplay
    Add settings button & canvas on pause menu
    Add music

--- Assets/Scripts/PlayerFunctions.cs
+++ Assets/Scripts/PlayerFunctions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:186ed101c5bbc6c9ef6ad0d2141a04d0200305d8de0af8a02e2bfab18e1797d5
-size 3939
+oid sha256:bcf86421f9fbd85a756f7bd5a1cadd7c779e5534f2af50b12e8aaae3cecaa9cf
+size 3949
